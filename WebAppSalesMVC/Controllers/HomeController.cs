2d7f7e7f9d2860c1f630bdff3fbcf41dace50436
Helielton Fioramonte <helifiora@gmail.com>
2019-07-15 21:09:13 -0300

    First Model-Controller-View

--- WebAppSalesMVC/Controllers/HomeController.cs
+++ WebAppSalesMVC/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebAppSalesMVC.Models;
+using WebAppSalesMVC.Models.ViewModels;
 
 namespace WebAppSalesMVC.Controllers
 {
