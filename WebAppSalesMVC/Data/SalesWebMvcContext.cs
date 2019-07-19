7e4b4e089f8481b427715ee28d024597ed25f8af
Helielton Fioramonte <helifiora@gmail.com>
2019-07-19 16:38:44 -0300

    Other entities and second migration

--- WebAppSalesMVC/Data/SalesWebMvcContext.cs
+++ WebAppSalesMVC/Data/SalesWebMvcContext.cs
@@ -10,5 +10,9 @@ namespace WebAppSalesMVC.Data
         }
         
         public DbSet<Department> Department { get; set; }
+
+        public DbSet<Seller> Seller { get; set; }
+        
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ WebAppSalesMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+namespace WebAppSalesMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+    }
+}
